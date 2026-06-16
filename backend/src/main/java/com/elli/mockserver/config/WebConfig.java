8bfb61755f0c0f4fe8f2984878a4960de8a221cd
Darl Ellison Floresca <floresca.darlellisong@gmail.com>
2026-06-16 20:18:30 +0800

    fix: update CORS configuration to remove trailing slash from allowed origin

--- backend/src/main/java/com/elli/mockserver/config/WebConfig.java
+++ backend/src/main/java/com/elli/mockserver/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://quickroute.degf.workers.dev/")
+                .allowedOrigins("https://quickroute.degf.workers.dev")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
