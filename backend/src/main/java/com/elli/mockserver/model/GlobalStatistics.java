1d195dc015630b53c2786c382b51cb366cc8f488
Darl Ellison Floresca <floresca.darlellisong@gmail.com>
2026-08-13 05:49:13 +0800

    remove lombok fix

--- backend/src/main/java/com/elli/mockserver/model/GlobalStatistics.java
+++ backend/src/main/java/com/elli/mockserver/model/GlobalStatistics.java
@@ -3,23 +3,49 @@ package com.elli.mockserver.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "global_statistics")
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class GlobalStatistics {
+
     @Id
     private LocalDate date;
     
     private long totalMocksCreated;
     private long totalRequestsServed;
+
+    public GlobalStatistics() {
+    }
+
+    public GlobalStatistics(LocalDate date, long totalMocksCreated, long totalRequestsServed) {
+        this.date = date;
+        this.totalMocksCreated = totalMocksCreated;
+        this.totalRequestsServed = totalRequestsServed;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public long getTotalMocksCreated() {
+        return totalMocksCreated;
+    }
+
+    public void setTotalMocksCreated(long totalMocksCreated) {
+        this.totalMocksCreated = totalMocksCreated;
+    }
+
+    public long getTotalRequestsServed() {
+        return totalRequestsServed;
+    }
+
+    public void setTotalRequestsServed(long totalRequestsServed) {
+        this.totalRequestsServed = totalRequestsServed;
+    }
 }
